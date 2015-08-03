531df444bce4e18d4b1aec352a5a92d48977969a
Justin Ryan <juryan@ziprealty.com>
2015-08-03 09:26:39 -0700

    initial cookbook gen.

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'zip_app'
+maintainer       'Justin Alan Ryan (ZipRealty / Realogy)'
+maintainer_email 'juryan@ziprealty.com'
+license          'Apache 2.0'
+description      'Installs/Configures zip_app'
+long_description 'Installs/Configures zip_app'
+version          '0.1.0'
